7c4f87951e05148e44333e2fe34883446ef2d95d
Stuart Williams <spookdejur@hotmail.com>
2025-12-30 21:38:17 -0800

    Config, Reader, 1

---
+++ Blitzkrieg.QuestV4.Lib.Configuration.Test/Test_Configuration.cs
@@ -0,0 +1,106 @@
+﻿namespace Blitzkrieg.QuestV4.Lib.Configuration.Test;
+
+[TestClass]
+public sealed class Test_Configuration
+{
+    /// <summary>
+    /// File to read
+    /// </summary>
+    const string ConfigurationFilename = @"./settings.json";
+    public TestContext TestContext { get; set; }
+
+    /// <summary>
+    /// QuestConfiguration under test
+    /// </summary>
+    public QuestConfigurationRoot QuestConfiguration { get; set; } = new QuestConfigurationRoot();
+
+    [TestInitialize]
+    public  void TestInit()
+    {
+        QuestConfiguration = ConfigurationJsonReader.FromFile(ConfigurationFilename);
+    }
+
+    [TestMethod]
+    public void IsLoaded()
+    {
+        Assert.IsNotNull(QuestConfiguration);
+    }
+
+    [TestMethod]
+    public void HasMonsters()
+    {
+        Assert.IsTrue(QuestConfiguration.Monsters.Any());
+    }
+
+    [TestMethod]
+    public void HasPotions()
+    {
+        Assert.IsTrue(QuestConfiguration.Potions.Any());
+    }
+
+    [TestMethod]
+    public void HasScrolls()
+    {
+        Assert.IsTrue(QuestConfiguration.Scrolls.Any());
+    }
+
+    [TestMethod]
+    public void HasStats()
+    {
+        Assert.IsTrue(QuestConfiguration.Stats.Any());
+    }
+
+    [TestMethod]
+    public void HasThingKinds()
+    {
+        Assert.IsTrue(QuestConfiguration.ThingKinds.Any());
+    }
+
+    [TestMethod]
+    public void HasThings()
+    {
+        Assert.IsTrue(QuestConfiguration.Things.Any());
+    }
+
+    [TestMethod]
+    public void Is_Cols_Ok()
+    {
+        Assert.IsGreaterThan(0, QuestConfiguration.MapInfo.Cols);
+    }
+
+
+    [TestMethod]
+    public void Is_Rows_Ok()
+    {
+        Assert.IsGreaterThan(0, QuestConfiguration.MapInfo.Rows);
+    }
+
+
+    [TestMethod]
+    public void Is_MinLevel_Ok()
+    {
+        Assert.IsGreaterThan(0, QuestConfiguration.MapInfo.MinLevel);
+    }
+
+
+    [TestMethod]
+    public void Is_MaxLevel_Ok()
+    {
+        Assert.IsGreaterThan(0, QuestConfiguration.MapInfo.MaxLevel);
+    }
+
+
+    [TestMethod]
+    public void Is_Folders_Ok()
+    {
+        Assert.IsFalse(string.IsNullOrWhiteSpace(QuestConfiguration.MapInfo.Folder));
+    }
+
+    [TestMethod]
+    public void Is_FolderExt_Ok()
+    {
+        Assert.IsFalse(string.IsNullOrWhiteSpace(QuestConfiguration.MapInfo.FileExt));
+    }
+
+
+}
---
+++ Blitzkrieg.QuestV4.Lib.Configuration/ConfigurationModel.cs
@@ -0,0 +1,160 @@
+﻿using Blitzkrieg.QuestV4.Lib.Configuration;
+using Newtonsoft.Json;
+
+namespace Blitzkrieg.QuestV4.Lib.Configuration
+{
+    // QuestConfigurationRoot myDeserializedClass = JsonConvert.DeserializeObject<QuestConfigurationRoot>(myJsonResponse);
+    public class MapInfo
+    {
+        [JsonProperty(nameof(Rows))]
+        public int Rows { get; set; }
+
+        [JsonProperty(nameof(Cols))]
+        public int Cols { get; set; }
+
+        [JsonProperty(nameof(MinLevel))]
+        public int MinLevel { get; set; }
+
+        [JsonProperty(nameof(MaxLevel))]
+        public int MaxLevel { get; set; }
+
+        [JsonProperty(nameof(Folder))]
+        public string Folder { get; set; }
+
+        [JsonProperty(nameof(FileExt))]
+        public string FileExt { get; set; }
+    }
+
+    public class Monster
+    {
+        [JsonProperty(nameof(Symbol))]
+        public string Symbol { get; set; }
+
+        [JsonProperty(nameof(Name))]
+        public string Name { get; set; }
+
+        [JsonProperty(nameof(MinLevel))]
+        public int MinLevel { get; set; }
+
+        [JsonProperty(nameof(MaxLevel))]
+        public int MaxLevel { get; set; }
+
+        [JsonProperty(nameof(Attack))]
+        public string Attack { get; set; }
+
+        [JsonProperty(nameof(Defense))]
+        public int Defense { get; set; }
+
+        [JsonProperty(nameof(Movement))]
+        public int Movement { get; set; }
+
+        [JsonProperty(nameof(Hits))]
+        public int Hits { get; set; }
+
+        [JsonProperty(nameof(Special))]
+        public string Special { get; set; }
+
+        [JsonProperty(nameof(LevelMod))]
+        public double LevelMod { get; set; }
+    }
+
+    public class Potion
+    {
+        [JsonProperty(nameof(Name))]
+        public string Name { get; set; }
+
+        [JsonProperty(nameof(Effect))]
+        public string Effect { get; set; }
+
+        [JsonProperty(nameof(Impact))]
+        public int Impact { get; set; }
+
+        [JsonProperty(nameof(Radius))]
+        public int Radius { get; set; }
+    }
+
+
+    public class Scroll
+    {
+        [JsonProperty(nameof(Name))]
+        public string Name { get; set; }
+
+        [JsonProperty(nameof(Effect))]
+        public string Effect { get; set; }
+
+        [JsonProperty(nameof(Impact))]
+        public int Impact { get; set; }
+
+        [JsonProperty(nameof(Radius))]
+        public int Radius { get; set; }
+    }
+
+    public class Stat
+    {
+        [JsonProperty(nameof(Id))]
+        public string Id { get; set; }
+
+        [JsonProperty(nameof(Name))]
+        public string Name { get; set; }
+
+        [JsonProperty(nameof(Min))]
+        public int Min { get; set; }
+
+        [JsonProperty(nameof(Max))]
+        public int Max { get; set; }
+    }
+
+    public class Thing
+    {
+        [JsonProperty(nameof(Name))]
+        public string Name { get; set; }
+
+        [JsonProperty(nameof(Unicode))]
+        public string Unicode { get; set; }
+
+        [JsonProperty(nameof(Kind))]
+        public int Kind { get; set; }
+    }
+
+    public class ThingKind
+    {
+        [JsonProperty(nameof(Name))]
+        public string Name { get; set; }
+
+        [JsonProperty(nameof(Value))]
+        public int Value { get; set; }
+
+        [JsonProperty(nameof(Note))]
+        public string Note { get; set; }
+
+        [JsonProperty(nameof(Kind))]
+        public int Kind { get; set; }
+    }
+
+
+
+    public class QuestConfigurationRoot
+    {
+        [JsonProperty(nameof(MapInfo))]
+        public MapInfo MapInfo { get; set; }
+
+        [JsonProperty(nameof(ThingKinds))]
+        public List<ThingKind> ThingKinds { get; } = [];
+
+        [JsonProperty(nameof(Things))]
+        public List<Thing> Things { get; } = [];
+
+        [JsonProperty(nameof(Scrolls))]
+        public List<Scroll> Scrolls { get; } = [];
+
+        [JsonProperty(nameof(Potions))]
+        public List<Potion> Potions { get; } = [];
+
+        [JsonProperty(nameof(Monsters))]
+        public List<Monster> Monsters { get; } = [];
+
+        [JsonProperty(nameof(Stats))]
+        public List<Stat> Stats { get; } = [];
+    }
+
+}
