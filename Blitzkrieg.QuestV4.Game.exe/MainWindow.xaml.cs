64aafa97bd03d75a5a79695f87d44b6433e5ebab
Stuart Williams <spookdejur@hotmail.com>
2026-01-18 12:20:45 -0800

    map, 7d

--- Blitzkrieg.QuestV4.Game.exe/MainWindow.xaml.cs
+++ Blitzkrieg.QuestV4.Game.exe/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 
+using Blitzkrieg.QuestV4.Lib.Models;
+
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -20,14 +22,39 @@ using Windows.Foundation.Collections;
 
 namespace Blitzkrieg.QuestV4.Game.exe
 {
+
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        public const int GRID_SIZE = 50;
+
         public MainWindow()
         {
             InitializeComponent();
+            grdBoard.BorderThickness = new Thickness(1);
+            grdBoard.ColumnSpacing = 0;
+            grdBoard.RowSpacing = 0;
+            grdBoard.HorizontalAlignment = HorizontalAlignment.Left;
+            grdBoard.VerticalAlignment = VerticalAlignment.Top;
+            grdBoard.Visibility = Visibility.Visible;
+            grdBoard.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 200, 200, 200));
+            grdBoard.RowDefinitions.Clear();
+            grdBoard.ColumnDefinitions.Clear();
+            for (int row = 0; row < MapLevel.MapRowsDefault; row++)
+            {
+                var rd =  new RowDefinition();
+                rd.Height = new GridLength(GRID_SIZE, GridUnitType.Pixel);
+              
+                grdBoard.RowDefinitions.Add(rd);
+            }
+            for (int col = 0; col < MapLevel.MapColsDefault; col++)
+            {
+                var cd = new ColumnDefinition();
+                cd.Width  = new GridLength(GRID_SIZE, GridUnitType.Pixel);
+                grdBoard.ColumnDefinitions.Add(cd);
+            }
         }
     }
 }
